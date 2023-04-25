007e298cfa18a8f7dd72f30f720492bd61fc76e7
daniel <daecgu@gmail.com>
2023-04-25 16:55:33 +0200

    Estructura de la web y CSS

--- test.py
+++ test.py
@@ -13,34 +13,50 @@ app = Flask(__name__)
 # app.config('SECRET_KEY') = 'erjkqhfvdnia(k)edjriopq372846%78342yr78hgfuihnusy78r3qurefanueyr7q238ehuryqwe78rpqroueiz$'
 
 # Configuramos SQLAlchemy para que se comunique con nuestra base de datos Postgre
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://daniel:asir23@Localhost:5432/proyectodb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://daniel:asir23@Localhost:5432/proyectodb'
 # Como ahora es un entorno de pruebas establecemos que no se nos envíe una señal cada vez que realizamos cambios.
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Hacemos que el ORM se utilice en la app
 db.init_app(app)
 
 # Creamos el objeto de la clase LoginManager que llamamos login_manager. Esto permitirá el control de sesiones.
 login_manager.init_app(app)
-login_manager.login_view="login"
+login_manager.login_view = "login"
+
 
 # Utilizamos esto para crear tabla por convención se llama create_table. Necesitamos utilizar uan función
 # llamada create_all con la instancia de sqlalchemy (db).
 # El decorador hace que se llame solo la primera vez
-@app.before_first_request
+""""@app.before_first_request
 def create_table():
     db.create_all()
-
+"""
 
 @app.route("/")
-def app_Proyecto():
+def app_proyecto():
     return render_template('index.html')
+
+
+@app.route('/acceso')
+def acceso():
+    return render_template("acceso.html")
+
+
+@app.route('/informacion')
+def informacion():
+    return render_template("informacion.html")
+
+
+@app.route('/registros')
+def registros():
+    return render_template("registros.html")
+
 
 """def hola():
     # return "<h1 style='color:blue'> Este test Funciona! </h1>" para la prueba inicial.
     # return "<h1 style='color:green'> Funciona gunicorn! </h1>"
     return "<h1 style='color:red'> Funciona NGINX! </h1>"""""
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

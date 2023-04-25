4e7e57ce329db830ebe68592a31f3e5560a5fa51
daniel <daecgu@gmail.com>
2023-04-25 12:48:24 +0200

    Otros archivos de configuración y actualización de los requisitos

---
+++ models.py
@@ -0,0 +1,35 @@
+# Importamos las librerías necesarias para poder guardar contraseñas de forma segura.
+from werkzeug.security import generate_password_hash, check_password_hash
+# Importamos las librerias para poder crear usuarios y gestionar el login.
+from flask_login import UserMixin, LoginManager
+# Importamos nuestro ORM para realizar los mapeos.
+from flask_sqlalchemy import SQLAlchemy
+
+# Para poder realizar la gestion de logins
+login_manager = LoginManager()
+# Para poder realizar mapeos
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    # Indicamos dónde se va a guardar
+    __tablename__ = "Usuarios"
+
+    id = db.Column(db.String(9), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(1024), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    desc = db.Column(db.String(1024), nullable=False)
+
+    # Generar un Hash para almacenar en la base de datos y no almacenar contraseñas.
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
--- test.py
+++ test.py
@@ -1,7 +1,35 @@
+# Importamos el ORM
+from flask_sqlalchemy import SQLAlchemy
+# Importamos Flask
 from flask import Flask
+# Importamos para gestionar loggin
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+# Importamos de los models los el usuario, la instancia del orm y el login manager.
+from models import User, login_manager, db
 
 # Creamos una instancia Flask que se llama app
 app = Flask(__name__)
+# Establecemos seguridad para nuestro programa:
+app.config('SECRET_KEY') = 'erjkqhfvdnia(k)edjriopq372846%78342yr78hgfuihnusy78r3qurefanueyr7q238ehuryqwe78rpqroueiz$'
+
+# Configuramos SQLAlchemy para que se comunique con nuestra base de datos Postgre
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://USUARIO:CONTRASEÑA@Localhost:5432/NOMBREDATABASE'
+# Como ahora es un entorno de pruebas establecemos que no se nos envíe una señal cada vez que realizamos cambios.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+# Hacemos que el ORM se utilice en la app
+db.init_app(app)
+
+# Creamos el objeto de la clase LoginManager que llamamos login_manager. Esto permitirá el control de sesiones.
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+# Utilizamos esto para crear tabla por convención se llama create_table. Necesitamos utilizar uan función
+# llamada create_all con la instancia de sqlalchemy (db).
+# El decorador hace que se llame solo la primera vez
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 
 @app.route("/")
@@ -9,6 +37,7 @@ def hola():
     # return "<h1 style='color:blue'> Este test Funciona! </h1>" para la prueba inicial.
     # return "<h1 style='color:green'> Funciona gunicorn! </h1>"
     return "<h1 style='color:red'> Funciona NGINX! </h1>"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
